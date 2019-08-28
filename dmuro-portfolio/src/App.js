d00f5c5b6e6c14c0c701a7e5cb6327b8d51e16de
Daykel Muro <Daykel_Muro@ryder.com>
2019-08-28 18:14:18 -0400

    cd/ci integration

--- dmuro-portfolio/src/App.js
+++ dmuro-portfolio/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <h1>Just confusion is what is happening</h1>
         <h1>React is Fun</h1>
         <h1>Just confusion is what is happening</h1>
+        <h1>Just confusion is what is happening</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
