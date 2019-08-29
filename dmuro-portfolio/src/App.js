84b6883ee1d71cd71a81fdbe4268f8f27ab661cd
daykelms <daykelms@gmail.com>
2019-08-28 23:06:21 -0400

    new content added

--- dmuro-portfolio/src/App.js
+++ dmuro-portfolio/src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Layout, Header, Navigation,Drawer, Content} from 'react-mdl';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Just Learning More in Azure</h1>
-           <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Daykel Portfolio
-        </a>
-      </header>
+    <div style={{ height: "300px", position: "relative" }}>
+      <Layout
+        style={{
+          background:
+            "url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover"
+        }}
+      >
+        <Header transparent title="Title" style={{ color: "white" }}>
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content />
+      </Layout>
     </div>
   );
 }
